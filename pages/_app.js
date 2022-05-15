597e23bf1544bb81c30732c24b1804b12f1aa267
aksinghrajpoot <imaksinghrajpoot@gmail.com.com>
2022-05-15 13:27:38 +0530

    -

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <link rel='shortcut icon' type='image/x-icon' href='/logo.png'/>
+    <title>TailNext Boilerplate</title>
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
