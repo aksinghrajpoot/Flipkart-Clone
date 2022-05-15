6195563565a9906d07f6be737ab52698e6043a6b
aksinghrajpoot <imaksinghrajpoot@gmail.com.com>
2022-05-15 13:20:43 +0530

    -

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding:{
+        DEFAULT:'1rem'
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
