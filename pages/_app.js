7af9c17d79694d9f70801955274caadbde6640d6
aksinghrajpoot <imaksinghrajpoot@gmail.com.com>
2022-05-16 08:13:39 +0530

    -

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return <>
   <Head>
-    <link rel='shortcut icon' type='image/x-icon' href='/logo.png'/>
-    <title>TailNext Boilerplate</title>
+    <link rel='shortcut icon' type='image/x-icon' href='/logo.jpg'/>
+    <title>Flipkart Shopping Website</title>
   </Head>
   <Component {...pageProps} />
   </>
